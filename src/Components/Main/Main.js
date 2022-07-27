9512aedb28ae6ed7eeaa1f611dbbf5a3b2cde474
eun-zi <rladmswlek@naver.com>
2022-07-27 12:27:59 +0900

    fix: 배포환경 설정 & 네비바 수정

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import ActiveCategory from "../../Redux/Actions/MainAction";
 
 function Main() {
+  const dispatch = useDispatch();
+
+  dispatch(ActiveCategory("main"));
+
   return <div>Main</div>;
 }
 
